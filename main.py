f2b2090bcf30d9b27f603bfd0deb20f8a90de3f3
ahumyck <ahumyck@gmail.com>
2021-04-08 04:12:25 +0400

    code refactor abstract class Handler added for images handling like adding noise or moving them abstract method experiment added for code generalization

--- main.py
+++ main.py
@@ -1,130 +1,44 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 
-import matcher
+import experiment
+from handler import SaltAndPepperHandler, MoveByHandler
 
 WHITE = 255
 GRAY = 127
 BLACK = 0
 
 
-def move_image_by(img, by_x, by_y, color):
-    """
-        Перемещаем исходное изображение by_x пикселей по горизонтали
-        и by_y пикселей по вертикали
-        "освободившееся" пространство заполняем указанным цветом
-    """
-    h, w = img.shape  # размер изображения
-    copy = np.copy(img)  # создаем копию
-    copy[by_x:, by_y:] = copy[:h - x, :w - y]  # сдвигаем
-    # освободившееся место заполняем указанным цветом
-    copy[:by_x, :] = color
-    copy[:, :by_y] = color
-    return copy
-
-
-def salt_and_pepper(image, s_vs_p=0.5, amount=0.004):
-    """
-        Функция для зашумления изображения способов
-        "соль и перец"
-    """
-    out = np.copy(image)
-    # Salt mode
-    num_salt = np.ceil(amount * image.size * s_vs_p)
-    coords = [np.random.randint(0, i - 1, int(num_salt))
-              for i in image.shape]
-    out[coords] = 1
-
-    # Pepper mode
-    num_pepper = np.ceil(amount * image.size * (1. - s_vs_p))
-    coords = [np.random.randint(0, i - 1, int(num_pepper))
-              for i in image.shape]
-    out[coords] = 0
-    return out
-
-
-def make_move_experiment(im1, im2, by_x, by_y):
-    """
-        Эксперимент для нахождения
-        одинаковых ключевых точек у оригинального и сдвинутого изображеия
-    """
-    moved_im2 = move_image_by(im2, by_x, by_y, BLACK)  # сдвигаем изображение
-
-    sift = cv2.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(im1, None)
-    kp2, des2 = sift.detectAndCompute(moved_im2, None)
-
-    matches = matcher.match_by_bruteforce_correct(kp1, des1, kp2, des2, x, y)  # ищем совпадения
-    img3 = matcher.drawMatches(im1, kp1, moved_im2, kp2, matches)  # рисуем совпадения
-    plt.imshow(img3)
-    plt.savefig(f'move_by/x = {by_x}, y = {by_y}.png')
-
-
-def salt_and_pepper_experiment(im1, im2, sp, amount):
-    """
-        Эксперимент для нахождения
-        одинаковых ключевых точек у оригинального и зашумленного изображеия
-    """
-    sp_im2 = salt_and_pepper(im2, sp, amount)
-
-    sift = cv2.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(im1, None)
-    kp2, des2 = sift.detectAndCompute(sp_im2, None)
-
-    matches = matcher.match_by_bruteforce_correct(kp1, des1, kp2, des2)
-    img3 = matcher.drawMatches(im1, kp1, sp_im2, kp2, matches)
-    plt.imshow(img3)
-    plt.savefig(f'salt and pepper/probability = {round(sp, 2)}, amount = {round(amount, 4)}.png')
-
-
-def salt_then_move_experiment(im1, im2, by_x, by_y, sp, amount):
-    """
-        Эксперимент для нахождения
-        одинаковых ключевых точек у оригинального и зашумленного и сдвинутого изображеия
-    """
-    sp_move_im2 = move_image_by(salt_and_pepper(im2, sp, amount), by_x, by_y, BLACK)
-
-    sift = cv2.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(im1, None)
-    kp2, des2 = sift.detectAndCompute(sp_move_im2, None)
-
-    matches = matcher.match_by_bruteforce_correct(kp1, des1, kp2, des2)
-    img3 = matcher.drawMatches(im1, kp1, sp_move_im2, kp2, matches)
-    plt.imshow(img3)
-    plt.savefig(f'move and salt/x = {by_x}, y = {by_y}, probability = {round(sp, 2)}, amount = {round(amount, 4)}.png')
-
-
 def float_range(start, stop, step):
     """
         Аналог range для float чисел
     """
-    float_r = []
-    while start < stop:
-        float_r.append(start)
-        start += step
-    return float_r
+    if stop < start:
+        start, stop = stop, start
+
+    for i in range(int(((stop - start) / step))):
+        yield start + i * step
 
 
 if __name__ == '__main__':
     img1 = cv2.imread('box.png', cv2.IMREAD_GRAYSCALE)  # train image
     img2 = cv2.imread('box.png', cv2.IMREAD_GRAYSCALE)  # test image
 
+    move_by_handler = MoveByHandler(img2)
+
     print('move experiments')
     for x in range(0, 50, 10):
         for y in range(0, 50, 10):
-            make_move_experiment(img1, img2, x, y)
+            experiment.make_experiment_fast(img1, move_by_handler.handle(x, y, BLACK),
+                                            f'move_by/x = {x}, y = {y}.png', th=10)
+
+    noise_handler = SaltAndPepperHandler(img2)
 
     print('s&p experiments')
-    for s_vs_p in float_range(0.1, 0.7, 0.1):
-        for amount in float_range(0.01, 0.7, 0.1):
-            salt_and_pepper_experiment(img1, img2, s_vs_p, amount)
 
-    print('moved and salted')
-    for x in range(0, 50, 10):
-        for y in range(0, 50, 10):
-            for s_vs_p in float_range(0.3, 0.7, 0.1):
-                for amount in float_range(0.3, 0.7, 0.1):
-                    salt_then_move_experiment(img1, img2, x, y, s_vs_p, amount)
+    for sp in float_range(0.1, 0.7, 0.1):
+        for amount in float_range(0.05, 0.4, 0.05):
+            experiment.make_experiment_fast(img1, noise_handler.handle(sp, amount),
+                                            f'salt and pepper/probability = {round(sp, 2)}, '
+                                            f'amount = {round(amount, 4)}.png', th=50)
 
     print('done')
--- matcher.py
+++ matcher.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 import cv2
 import numpy as np
 
@@ -11,44 +13,50 @@ def create_match(distance, trainIdx, queryIdx, imgIdx=0):
     return match
 
 
-def match_by_bruteforce_correct(kp1, des1, kp2, des2, x=0, y=0, limit=25):
-    """
-        Ищем совпадения особых точек основываясь на знании о их месторасположении
-        Сначала мы проверяем, что сравниваемые точки сдвинута одна относительно другой на заданную величину
-        После чего считаем для совпадения L2 норму от разности дискрипторов
-    :param kp1: ключевые точки обучающего изображениея
-    :param des1: дискрипторы ключевых точек обучающего изображения
-    :param kp2: ключевые точки тестового изображениея
-    :param des2: дискрипторы ключевых точек тестового изображения
-    :param x: Смещение по оси х. Значение х=0 используется в эксперементах с шумами, когда мы не сдвигаем изображение
-    :param y: Смещение по оси у. Значение х=0 используется в эксперементах с шумами, когда мы не сдвигаем изображение
-    :param limit: ограниче сверху для кол-ва matcher'-ов
-    :return: список matcher'-ов
-    """
-    result = []
-    i = 0
-    for trainIdx, d1 in enumerate(des1):
-        for queryIdx, d2 in enumerate(des2):
-            keypoint1 = kp1[trainIdx]
-            keypoint2 = kp2[queryIdx]
-            if np.round(keypoint2.pt[1] - keypoint1.pt[1]) == x and \
-                    np.round(keypoint2.pt[0] - keypoint1.pt[0]) == y:
-                result.append(create_match(np.linalg.norm(d1 - d2), trainIdx, queryIdx, 0))
-                i += 1
-                if i >= limit:
-                    return np.array(result, dtype=cv2.DMatch)
-                break
-    return np.array(result, dtype=cv2.DMatch)
-
-
-def match_by_bruteforce_fast(des1, des2, th=10, limit=25):
+def match_by_bruteforce_min_norm(kp1, des1, kp2, des2, dx=40, dy=40, th=10):
     """
         Ищем совпадения особых точек считая L2 норму разницы дискрипторов
         Если L2 норма меньше некоторого порогового значения, то считаем что ключевые точки совпадают, иначе нет
+        Из всех подходящех совпадений выбираем совпадения с минимальной нормой
+    :param kp1: ключевые точки обучающего изображениея
+    :param kp2: ключевые точки тестового изображениея
+    :param des1: дискрипторы ключевых точек обучающего изображения
+    :param des2: дискрипторы ключевых точек тестового изображения
+    :param th: порог для L2 нормы
+    :param dx: максимальная разность координат точек по оси х
+    :param dy: максимальная разность координат точек по оси у
+    :return: список matcher'-ов
+    """
+    possible_matches_array = []
+    for trainIdx, d1 in enumerate(des1):
+        possible_matches = []
+        for queryIdx, d2 in enumerate(des2):
+            d = np.linalg.norm(d1 - d2)
+            if d <= th:
+                keypoint1 = kp1[trainIdx]
+                keypoint2 = kp2[queryIdx]
+                if np.abs(np.round(keypoint2.pt[1] - keypoint1.pt[1])) <= dx and \
+                        np.abs(np.round(keypoint2.pt[0] - keypoint1.pt[0])) <= dy:
+                    possible_matches.append(create_match(d, trainIdx, queryIdx, 0))
+        if len(possible_matches) > 0:
+            possible_matches_array.append(possible_matches)
+
+    return np.array([min(possible_matches, key=attrgetter('distance')) for possible_matches in possible_matches_array],
+                    dtype=cv2.DMatch)
+
+
+def match_by_bruteforce_fast(kp1, des1, kp2, des2, dx=40, dy=40, th=10, limit=25):
+    """
+        Ищем совпадения особых точек считая L2 норму разницы дискрипторов
+        Если L2 норма меньше некоторого порогового значения, то считаем что ключевые точки совпадают, иначе нет
+    :param kp1: ключевые точки обучающего изображениея
+    :param kp2: ключевые точки тестового изображениея
     :param des1: дискрипторы ключевых точек обучающего изображения
     :param des2: дискрипторы ключевых точек тестового изображения
     :param th: порог для L2 нормы
     :param limit: ограниче сверху для кол-ва matcher'-ов
+    :param dx: максимальная разность координат точек по оси х
+    :param dy: максимальная разность координат точек по оси у
     :return: список matcher'-ов
     """
     i = 0
@@ -57,11 +65,15 @@ def match_by_bruteforce_fast(des1, des2, th=10, limit=25):
         for queryIdx, d2 in enumerate(des2):
             d = np.linalg.norm(d1 - d2)
             if d <= th:
-                result.append(create_match(d, trainIdx, queryIdx, 0))
-                i += 1
-                if i >= limit:
-                    return np.array(result, dtype=cv2.DMatch)
-                break
+                keypoint1 = kp1[trainIdx]
+                keypoint2 = kp2[queryIdx]
+                if np.abs(np.round(keypoint2.pt[1] - keypoint1.pt[1])) <= dx and \
+                        np.abs(np.round(keypoint2.pt[0] - keypoint1.pt[0])) <= dy:
+                    result.append(create_match(d, trainIdx, queryIdx, 0))
+                    i += 1
+                    if i >= limit:
+                        return np.array(result, dtype=cv2.DMatch)
+                    break
     return np.array(result, dtype=cv2.DMatch)
 
 
